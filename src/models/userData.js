1ebd3183b6a8a35ffc58db4e5ae9e30ed92d59cb
Siddharth Sameer Nevgi <siddharthnevgi@gmail.com>
2025-09-02 15:36:34 +0530

    Added signup/login backend with database integration

---
+++ src/models/userData.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userData = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
+const User =
+  mongoose.models.userDetails || mongoose.model("userDetails", userData);
+export default User;
